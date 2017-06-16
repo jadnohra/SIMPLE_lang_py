60780ea6363223d71ef338735b208d792559c8a5
Jad Nohra <jadnohra@users.noreply.github.com>
2017-06-16 21:55:18 +0200

    Added ul tuples

--- ulambda.py
+++ ulambda.py
@@ -30,17 +30,25 @@ def ul_inspect_depth(lbda):
 '''
 
 # lambdas
-ul_true = (lambda t: (lambda f: t))
-ul_false = (lambda t: (lambda f: f))
+ul_left = (lambda l: (lambda r: l))
+ul_right = (lambda l: (lambda r: r))
+ul_tuple = (lambda l: (lambda r: (lambda p: p(l)(r))))
+ul_ltup = (lambda tup: tup(ul_left))
+ul_rtup = (lambda tup: tup(ul_right))
+
 
 # convenience lambdas
+ul_true = ul_left
+ul_false = ul_right
 ul_N = lambda N: ul_meta_build_N(N)[0]
 ul_B = lambda B: ul_true if B else ul_false
 ul_if = lambda b: b
 ul_is0 = lambda n: n(lambda x: ul_false)(ul_true)
 
+
 def test1():
-  print '5', ul_decode_N(ul_N(5))
-  print 'False', ul_decode_B(ul_B(False))
-  print '0 is 0', ul_decode_B(ul_is0(ul_N(0)))
-  print '5 is 0', ul_decode_B(ul_is0(ul_N(3)))
+  print '5:', ul_decode_N(ul_N(5))
+  print 'False:', ul_decode_B(ul_B(False))
+  print '0 is 0:', ul_decode_B(ul_is0(ul_N(0)))
+  print '5 is 0:', ul_decode_B(ul_is0(ul_N(3)))
+  print 'right of (3,6):', ul_decode_N(ul_rtup(ul_tuple(ul_N(3))(ul_N(6))))
