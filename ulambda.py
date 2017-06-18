a33c6ccc2e1a61cf9e59c5d1067a859a1be1b23e
Jad Nohra <jadnohra@users.noreply.github.com>
2017-06-18 15:29:38 +0200

    Added _ul_mod using a Zcombinator-like approach

--- ulambda.py
+++ ulambda.py
@@ -67,11 +67,13 @@ ul_is0 = lambda n: n(lambda m: ul_false)(ul_true)
 # more complex lambdas
 ul_leq = (lambda n1: (lambda n2: ul_is0( ul_sub(n1)(n2) ) ))
 ul_less = (lambda n1: (lambda n2: ul_is0( ul_sub(ul_inc(n1))(n2) ) ))
-ul_mod_x = (lambda n1: (lambda n2: n2  ))
-ul_mod_bad = (lambda n1: (lambda n2: ( ul_leq(n1)(n2) )( n1 )( ul_mod(ul_sub(n1)(n2))(n2)  ) )) #this turns into an infinite recursion
-_ul_mod_v1 = (lambda n1: (lambda n2: ((ul_less(n1)(n2))(n2)(n1))(lambda m: (ul_less(m)(n2))(m)(ul_sub(m)(n2)) )(n1) ))
-ul_mod = _ul_mod_v1
-#TODO: one version with the same 'lazy eval' trick as bs_while_eval, another with the book's ideas as a chance to learn the first combinators.
+_ul_mod_v1 = (lambda n1: (lambda n2: ((ul_less(n1)(n2))(n2)(n1))(lambda m: (ul_less(m)(n2))(m)(ul_sub(m)(n2)) )(n1) )) #one trick to not have to recurse: iterate over the larger number
+#bad_ul_mod_naive = (lambda n1: (lambda n2: ( ul_leq(n1)(n2) )( n1 )( ul_mod_bad(ul_sub(n1)(n2))(n2)  ) )) #this turns into an infinite recursion
+#bad_ul_mod_v2_rec = (lambda rec: (lambda n1: (lambda n2: ( ul_leq(n1)(n2) )( n1 )( _ul_mod_v2_rec(rec)(ul_sub(n1)(n2))(n2)  ) )) )
+#bad_ul_mod_v2 = _ul_mod_v2_rec(_ul_mod_v2_rec) #this does not help either
+ul_comile_rec = lambda cond: lambda l: lambda r: cond(l)(r)
+_ul_mod_v2 = (lambda n1: (lambda n2: ul_comile_rec(ul_less(n1)(n2))(lambda: n1)(lambda: _ul_mod_v2(ul_sub(n1)(n2))(n2) )() ) ) # I think this is a Z combinator
+ul_mod = _ul_mod_v2
 
 def test1():
   print '5:', ul_decode_N(ul_N(5))
@@ -96,8 +98,8 @@ def test1():
   print '3 % 3:', ul_decode_N(ul_mod(ul_N(3))(ul_N(3)) )
 
 
-def test_fizzbuzz():
+def test_fizzbuzz(mx = 30):
   ul_mod_print = lambda s: lambda a: lambda n: ul_is0(ul_mod(n)(a))(lambda: ul_out(n, s))(lambda: ul_out(n, ''))()
   prog_ul_fizzbuzz = lambda mx: ( ul_N(mx)(lambda m: ul_inc( ul_mod_print('FizzBuzz ')(ul_N(15))(ul_mod_print('Buzz ')(ul_N(5))( ul_mod_print('Fizz ')(ul_N(3))(m)) ) )) ( ul_N(0) ) )
-  prog_ul_fizzbuzz(30)
+  prog_ul_fizzbuzz(mx)
   print ''
